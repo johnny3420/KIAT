d503b6b39274567b527cfc90ff1f02c4166ab593
John <jtdavis@ucdavis.edu>
2016-10-04 14:59:41 -0700

    more mapping statistics

--- Aligner_Comparison/Aligner_comparison_plots.R
+++ Aligner_Comparison/Aligner_comparison_plots.R
@@ -8,3 +8,13 @@ ggplot(data=melt.stats, aes(Aligner,Mapped, fill=Aligner)) +
   facet_wrap("Alignment") + 
   geom_bar(stat = "identity")
 #ggsave("Aligner_comparison_barplot.png")
+
+m.stats <- read.delim("Aligner_Comparison/modified.aligner.comparison.tab", header = T, sep = "\t")
+melt.m.stats <- melt(m.stats)
+colnames(melt.m.stats) <- c("Sample","Aligner","Mapping","Percentage")
+
+ggplot(data=melt.m.stats, aes(Mapping,Percentage, fill=Mapping)) + 
+  facet_grid(Sample ~ Aligner) + 
+  geom_bar(stat = "identity")
+
+ggsave("modified.aligner_comparison_barplot.png")
